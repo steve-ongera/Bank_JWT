807ac1af07319d4a19cb851348f35ad5edd94291
steve-ongera <gadafisteve001@gmail.com>
2026-06-20 12:41:37 +0300

    made updates

--- backend_bank/bank_Jwt/admin.py
+++ backend_bank/bank_Jwt/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Transaction
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'balance', 'created_at')
+    search_fields = ('email', 'name')
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'amount', 'balance_after', 'created_at')
+    list_filter = ('type', 'created_at')
+    search_fields = ('user__email', 'description')
+    readonly_fields = ('created_at',)
